bc8a4c3f0ff8dca226af855a9092ab5be9796bc0
SEUN <adeniyi.learn@gmail.com>
2022-12-09 08:19:24 -0800

    lowerApha

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ * Return: Always return 0
+ *
+ *
+ */
+
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+		putchar(i);
+
+	for (i = 65; i < 91; i++)
+		putchar(i);
+
+	putchar(10);
+
+	return (0);
+}
