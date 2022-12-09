2382ce47c28d053b3e24f40cc8bff45f1d0a9efb
SEUN <adeniyi.learn@gmail.com>
2022-12-09 11:06:01 -0800

    first 10 digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - the program's main function
+ *
+ * Return: return 0 (successful)
+ */
+
+int main(void)
+{
+	int count;
+
+	for (count = 0; count < 10; count++)
+	{
+		printf("%d", count);
+	}
+	putchar('\n');
+	return (0);
+}
