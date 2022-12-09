9facf62569e32846ade3d5177e50a21a001c4b40
SEUN <adeniyi.learn@gmail.com>
2022-12-09 11:10:25 -0800

    digits with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - the program's main function
+ *
+ * Return: return 0 (successful)
+ */
+
+int main(void)
+{
+	int count;
+
+	for (count = 48; count < 58; count++)
+	{
+		putchar(count);
+	}
+	putchar('\n');
+	return (0);
+}
