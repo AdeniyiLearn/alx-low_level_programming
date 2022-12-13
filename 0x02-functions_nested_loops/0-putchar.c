ae044239564e084099ad3fc8bda340f02882d503
SEUN <adeniyi.learn@gmail.com>
2022-12-13 07:14:11 -0800

    initial commit for 0x02

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - the program's main function
+ *
+ * putchar: writes individual character to standard output one at a time
+ *
+ * Return: return 0 (successful)
+ */
+
+int main(void)
+{
+	char to_print[] = "_putchar";
+	int count;
+
+	for (count = 0; count < 8; count++)
+	{
+		putchar(to_print[count]);
+	}
+
+	putchar('\n');
+	return (0);
+}
