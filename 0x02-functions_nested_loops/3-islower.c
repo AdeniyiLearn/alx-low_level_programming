70c9a33a579341f0297b6e2cdff1a92478d51a58
SEUN <adeniyi.learn@gmail.com>
2022-12-13 08:09:00 -0800

    llll

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,14 +1,15 @@
-#include <stdio.h>
+#include <stdio.
 
 /**
- * int _islower(int c) - function checks if it is lower alphabet
+ * _islower - function checks if it is lower alphabet
+ * @c: integer parameter
  * Return: returns 1 if yes and 0 if no.
  */
 
 
 int _islower(int c)
 {
-	if ( c >= 97 && c <= 122)
+	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
@@ -17,4 +18,4 @@ int _islower(int c)
 	{
 		return (0);
 	}
- }
+}
