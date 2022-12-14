d016b5f55aaf862aebdca82b4106fc0485f2080c
SEUN <adeniyi.learn@gmail.com>
2022-12-14 15:04:46 -0800

    jjj

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
  * jack_bauer - function checks if it is an alphabet
  *
  * Return: returns Void
  */
-
-
 void jack_bauer(void)
 {
-	int i;
-	int j;
+	int i, j;
 
 	for (i = 0; i < 24; i++)
 	{
@@ -32,7 +28,6 @@ void jack_bauer(void)
 			putchar('\n');
 			}
 		}
-
 		if (i > 9)
 		{
 			for (j = 0; j < 60; j++)
