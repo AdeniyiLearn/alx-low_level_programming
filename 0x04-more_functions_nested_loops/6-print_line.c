27c42b829430f871205eef1f409de454a3c36461
SEUN <adeniyi.learn@gmail.com>
2022-12-18 14:22:31 -0800

    lll

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_line - function prints ___ nth times
+ *
+ * Return: Void
+ */
+
+void print_line(int n)
+{
+	int counter;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else if (n > 0)
+	{
+		for (counter = 0; counter < n; counter++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
