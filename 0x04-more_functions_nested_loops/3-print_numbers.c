e06a33595eeb8f89d6ec833bc7706d21632f895d
SEUN <adeniyi.learn@gmail.com>
2022-12-18 12:31:41 -0800

    zero to nine

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_numbers - function prints 0 to 9
+ *
+ * Return: Void
+ */
+
+void print_numbers(void)
+{
+	int c = 48;
+
+	while (c <= 57)
+	{
+		_putchar(c);
+		c++;
+	}
+}
