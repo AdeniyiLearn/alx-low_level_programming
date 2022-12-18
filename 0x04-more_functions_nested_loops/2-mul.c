cceefcf331852c77ed375ff31db870fb8ae6c4fe
SEUN <adeniyi.learn@gmail.com>
2022-12-18 12:23:48 -0800

    multiplies given parameters

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * mul - function multiplies given integers together
+ * @a: first integer parameter
+ * @b: Second integer parameter
+ * Return: returns result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
