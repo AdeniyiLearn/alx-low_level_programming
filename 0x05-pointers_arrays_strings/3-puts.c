6b8b1f3d2eba513fdbdf8bcbb6eed9f4add4d904
SEUN <adeniyi.learn@gmail.com>
2022-12-19 07:23:27 -0800

    just like puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts - function prints strings and prints a new line aftre
+ * @str: pointer to strings of character
+ *
+ * Return: Void
+ */
+
+void _puts(char *str)
+{
+	char try;
+
+	do {
+		try = *str;
+		_putchar(try);
+	} while (*str++);
+	_putchar('\n');
+}
