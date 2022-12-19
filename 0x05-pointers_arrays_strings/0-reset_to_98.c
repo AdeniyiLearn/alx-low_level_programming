f72a5fd120bdd02b18fc07d0627564f15cc2855f
SEUN <adeniyi.learn@gmail.com>
2022-12-19 04:58:09 -0800

    initial commmit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * reset_to_98 - function updates the value
+ * @n: integer parameter
+ *
+ * Return: Void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
