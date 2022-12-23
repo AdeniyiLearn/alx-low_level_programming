75338c0870109541ac1fd85e5c1298d338dfb7bd
SEUN <adeniyi.learn@gmail.com>
2022-12-22 20:49:58 -0800

    jjj

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * _strncpy - function appends strings from n bytes of source to destinaton
+ *
+ * @src: pointer to strings of character source
+ * @dest: pointer to strings of the character destination
+ * @n: total bytes of source to be appended
+ *
+ * Return: pointer to character of the destination
+ */
+
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a;
+
+	for (a = 0; a < n; a++)
+	{
+		*(dest + a) = *(src + a);
+	}
+	return (dest);
+}
+
+
+/**
+ * _strlen - function returns length
+ *
+ * @s: pounter to character
+ *
+ * Return: result
+ *
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*s++)
+	{
+		count++;
+	}
+	return (count);
+}
