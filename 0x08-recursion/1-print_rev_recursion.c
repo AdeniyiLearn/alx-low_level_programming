7fc0e34be0efa5b475fb7d37f198ca60383fc3ca
SEUN <adeniyi.learn@gmail.com>
2023-01-05 12:10:05 -0800

    first commit for task 01

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string and then a new line
+ *
+ * @s: pointer to a character parameter
+ * Return: void
+ *
+ */
+
+void _print_rev_recursion(char *s)
+{
+	char c, a;
+
+	c = *s++;
+	if (c == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_print_rev_recursion(s);
+		a = *s--;
+		_putchar(a);
+	}
+}
