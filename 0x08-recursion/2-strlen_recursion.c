08328489aac7a2b91643fbdb3dbb4d16d9569d56
SEUN <adeniyi.learn@gmail.com>
2023-01-07 10:48:08 -0800

    lll

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - prints a string and then a new line
+ *
+ * @s: pointer to a character parameter
+ * Return: result
+ *
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (sizeof(*s) + _strlen_recursion(s += 1));
+	}
+}
