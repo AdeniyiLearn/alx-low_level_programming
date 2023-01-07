2c0c965d22ddf2c293a4629bbfbb8cdffc5c96ab
SEUN <adeniyi.learn@gmail.com>
2023-01-07 11:00:57 -0800

    lll

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * is_prime_number - raise x to the power of y
+ *
+ * @n: integer parameter
+ *
+ *
+ * Return: -1 if less than 0, 1 if equals to 0 else result
+ *
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 0)
+	{
+		return (0);
+	}
+	else if (n % 2 == 0 || n % 3 == 0 || n % 5 == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
