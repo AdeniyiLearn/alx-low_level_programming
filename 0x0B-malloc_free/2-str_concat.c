a76c4cee090d2c9a2c5595e71aa2cc37ff3f8950
SEUN <adeniyi.learn@gmail.com>
2023-01-11 13:37:38 -0800

    lll

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * _concat - concatenates two strings
+ * @s1: string parameter
+ * @s2: 2nd string parameter
+ * Return: return NULL if 0, result if successful
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i, j, count, count2, size, size2, size3;
+	char *snd;
+
+	count = 0, count2 = 0, i = 0, j = 0, size = 0, size2 = 0, size3 = 0;
+	if (s1 == NULL || s2 == NULL)
+	{
+		return (NULL);
+	}
+	while (*(s1 + i) != '\0')
+	{
+		i++;
+		size++;
+	}
+	while (*(s2 + j) != '\0')
+	{
+		j++;
+		size2++;
+	}
+	size3 = size + size2;
+	snd = malloc((size3 + 1) * sizeof(char));
+	while (count < size)
+	{
+		snd[count] = *(s1 + count);
+		count++;
+	}
+	while (count < size3)
+	{
+		snd[count] = *(s2 + count2);
+		count++;
+		count2++;
+	}
+	return (snd);
+}
