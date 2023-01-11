4c9ee89218ae6550fb8d09a8d25544a9c37d9438
SEUN <adeniyi.learn@gmail.com>
2023-01-11 13:42:18 -0800

    lll

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _concat - concatenates two strings
+ * str_concat - concatenates two strings
  * @s1: string parameter
  * @s2: 2nd string parameter
  * Return: return NULL if 0, result if successful
