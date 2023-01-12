eb60dd55ed7ed7990aa94df43d30410d42011875
SEUN <adeniyi.learn@gmail.com>
2023-01-12 03:25:13 -0800

    lll

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function
+ * @argc: first parameter that takes count
+ * @argv: second parameter, an pointer to arguments to main
+ *
+ * Return: success
+ *
+ */
+
+int main(int argc, char **argv)
+{
+	int c;
+
+	if (argc < 1)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+
+	else
+	{
+		c = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", (c));
+	}
+
+	exit(EXIT_SUCCESS);
+}
