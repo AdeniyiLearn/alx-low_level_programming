0b21f76f5e573817bd4c8f3a5ca701a7eb610cba
SEUN <adeniyi.learn@gmail.com>
2023-01-12 02:19:27 -0800

    ll

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function
+ * @argc: first parameter that takes count
+ * @argv: second parameter, an pointer to arguments to main
+ *
+ */
+
+int main(int argc, char **argv)
+{
+        while(argc--)
+                printf("%s\n", *argv++);
+        exit(EXIT_SUCCESS);
+}
