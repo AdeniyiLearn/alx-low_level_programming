755f9363cfc24cf80033de36b78ff87a5fba0420
SEUN <adeniyi.learn@gmail.com>
2023-02-07 22:06:39 -0800

    number return

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * list_len - function returns number of elements
+ * @h: const parameter of type list
+ * Return: 0 and nil if NULL and number of nodes if successful
+ *
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+	const list_t *mover;
+
+	if (h == NULL)
+	{
+		return (1);
+	}
+
+	mover = h;
+	while (mover != NULL)
+	{
+
+		count++;
+		mover = mover->next;
+	}
+	return (count);
+}
