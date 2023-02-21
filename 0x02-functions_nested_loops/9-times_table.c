c872d48b77aabd7d5d65ed888aa09ca3e9535c86
SEUN <adeniyi.learn@gmail.com>
2023-02-21 04:09:49 -0800

    new files

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,49 +1,8 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * jack_bauer - function checks if it is an alphabet
+ * times_table - function checks if it is an alphabet
  *
  * Return: returns Void
  */
-void jack_bauer(void)
-{
-	int i, j;
 
-	for (i = 0; i < 24; i++)
-	{
-		if (i < 10)
-		{
-			for (j = 0; j < 60; j++)
-			{
-				if (j < 10)
-				{
-					printf("0%d", i);
-					printf(":0%d", j);
-				}
-				if (j > 9)
-				{
-					printf("0%d", i);
-					printf(":%d", j);
-				}
-			putchar('\n');
-			}
-		}
-		if (i > 9)
-		{
-			for (j = 0; j < 60; j++)
-			{
-				if (j < 10)
-				{
-					printf("%d", i);
-					printf(":0%d", j);
-				}
-				if (j > 9)
-				{
-					printf("%d", i);
-					printf(":%d", j);
-				}
-			putchar('\n');
-			}
-		}
-	}
-}
