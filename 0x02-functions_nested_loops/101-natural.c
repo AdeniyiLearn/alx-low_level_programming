e02ef4cfd7ab512f28e174aa0b09657cc3d1267c
SEUN <adeniyi.learn@gmail.com>
2023-02-22 02:30:47 -0800

    build: sum of multiples of 3 and 5 below 1024

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,20 +9,20 @@
 
 int main(void)
 {
-	int incNum, nNum, con1;
+	int incNum, nNum, con2;
 
 	incNum = 0;
 	nNum = 1024;
-	con1 = 0;
+	con2 = 0;
 
 	while (incNum <= nNum)
 	{
 		if (incNum % 3 == 0 || incNum % 5 == 0)
 		{
-			con1 += incNum;
+			con2+= incNum;
 		}
 		incNum++;
 	}
-	printf("%d\n", incNum);
+	printf("%d\n", con2);
 	return (0);
 }
