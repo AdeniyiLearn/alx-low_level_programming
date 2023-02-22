c9085b47c2add7b2320c69eafff06df873180b9d
SEUN <adeniyi.learn@gmail.com>
2023-02-21 22:07:53 -0800

    build: x_table to 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,4 +5,31 @@
  *
  * Return: returns Void
  */
+void times_table(void)
+{
+	int count, inc1, inc2, result;
 
+	count = 9;
+	inc1 = 0;
+	inc2 = 0;
+
+	while (inc1 <= count)
+	{
+		while (inc2 < count)
+		{
+			result = inc2 * inc1;
+			if (inc2 >= 1)
+			{
+				printf("%4d,", result);
+			}
+			else
+			{
+				printf("%d,", result);
+			}
+			inc2++;
+		}
+		printf("%4d\n", inc2 * inc1);
+		inc2 = 0;
+		inc1++;
+	}
+}
