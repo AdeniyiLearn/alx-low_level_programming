f1869af8f0a918fa0bdbe544b21f2d09986d8fa0
SEUN <adeniyi.learn@gmail.com>
2023-03-02 10:41:55 -0800

    hhh

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -16,7 +16,7 @@ char *_strncat(char *dest, char *src, int n)
 
 	dest_lent = _strlen(dest);
 	begin = dest_lent;
-	if (*src == '\0' || *dest == '\0' || n == '\0')
+	if (*src == '\0' || *dest == '\0' || n == 0)
 	{
 		exit(EXIT_FAILURE);
 	}
