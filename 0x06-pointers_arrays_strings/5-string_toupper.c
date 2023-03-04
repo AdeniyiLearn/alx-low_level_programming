0c883eb4180425914961f4ed6d96785fe999ea76
SEUN <adeniyi.learn@gmail.com>
2023-03-03 19:39:43 -0800

    build: reverse array elements

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * sring_toupper - function changes all lower case letters of a string to uppercase
+ *
+ * @a: the pointer the string
+ *
+ *
+ * Return: the updated character
+ */
+
+
+char *string_toupper(char *a)
+{
+
+}
