a3d98ac9cb2cec3eb68d72978d6e82d8f4c6c094
SEUN <adeniyi.learn@gmail.com>
2023-03-05 13:42:20 -0800

    build lower to upper of strings

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * sring_toupper - function changes all lower case letters of a string to uppercase
+ * string_toupper - changes all lower case letters of a string to uppercase
  *
  * @a: the pointer the string
  *
@@ -11,5 +11,15 @@
 
 char *string_toupper(char *a)
 {
+	int count = 0;
 
+	while (*(a + count) != '\0')
+	{
+		if (*(a + count) >= 97 && *(a + count) <= 122)
+		{
+			*(a + count) = *(a + count) - 32;
+		}
+		count++;
+	}
+	return (a);
 }
