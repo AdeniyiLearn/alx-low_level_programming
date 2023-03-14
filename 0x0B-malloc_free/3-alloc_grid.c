bf3faf497d86d655c58edcfb9a94ba5fbd6f8d44
SEUN <adeniyi.learn@gmail.com>
2023-03-13 20:44:21 -0700

    kkk

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * str_concat - concatenates two strings
+ * @s1: string parameter
+ * @s2: 2nd string parameter
+ * Return: return NULL if 0, result if successful
+ */
+int **alloc_grid(int width, int height);
+{
+	
+}
