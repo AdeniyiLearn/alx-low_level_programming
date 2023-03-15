34caa2cf92840469bbf7b1c06453feecc27f55d0
SEUN <adeniyi.learn@gmail.com>
2023-03-15 04:22:52 -0700

    create 2d array with double pointer

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,11 +1,23 @@
 #include "main.h"
 /**
- * str_concat - concatenates two strings
- * @s1: string parameter
- * @s2: 2nd string parameter
+ * alloc_grid - concatenates two strings
+ * @width: first parameter
+ * @height: 2nd string parameter
  * Return: return NULL if 0, result if successful
  */
-int **alloc_grid(int width, int height);
+int **alloc_grid(int width, int height)
 {
-	
+	int h_index;
+	int **p;
+
+	p = calloc(height, sizeof(int));
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	for (h_index = 0; h_index < height; h_index++)
+	{
+		p[h_index] = calloc(width, sizeof(int));
+	}
+	return (p);
 }
