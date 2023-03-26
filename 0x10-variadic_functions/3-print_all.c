7f301b9720b7d7825447347ce898c25d15194964
SEUN <adeniyi.learn@gmail.com>
2023-03-25 19:30:11 -0700

    test commit

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,58 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - function that prints anything
+ *
+ * @format:  string to be printed between numbers
+ *
+ * @...: represents all parameters
+ *
+ * Return: void
+ */
+
+void print_all(const char * const format, ...)
+{
+	int num, xi;
+	char x;
+	double xf;
+	char *xs;
+	va_list args;
+
+	num = 0;
+	va_start(args, format);
+    
+	while (format[num])
+	{
+		if (format[num] == '\0')
+		{
+			printf("(nil)");
+		}
+
+		switch (format[num])
+		{
+		case 'c' :
+			x = va_arg(args, int);
+			printf("%c, ", x);
+			break;
+		case 'i' :
+			xi = va_arg(args, int);
+			printf("%d, ", xi);
+			break;
+		case 'f' :
+			xf = va_arg(args, double);
+			printf("%f", xf);
+			break;
+		case 's' :
+			xs = va_arg(args, char *);
+			printf("%s", xs);
+			break;
+
+		default:
+			break;
+		}
+
+		num++;
+	}
+	printf("\n");
+	va_end(args);
+}
