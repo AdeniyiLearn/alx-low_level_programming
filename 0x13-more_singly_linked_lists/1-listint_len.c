a3ea6400ee882048e7fec1b03523a016fd00ef3a
SEUN <adeniyi.learn@gmail.com>
2023-04-04 20:23:00 -0700

    edit task 1: trasverse

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -9,12 +9,7 @@ size_t listint_len(const listint_t *h)
 {
 	int node_num = 0;
 
-	if (h == NULL)
-	{
-		exit(1);
-	}
-
-	while (h != NULL && h->n != '\0')
+	while (h)
 	{
 		node_num++;
 		h = h->next;
