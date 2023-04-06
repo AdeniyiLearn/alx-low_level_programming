f064092cda0bfc304dca3882e594b8f1fdcf0c38
SEUN <adeniyi.learn@gmail.com>
2023-04-05 21:20:05 -0700

    initial commit

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * free_listint - This function frees a list
+ * @head: takes in the address to a head node that pounts to NULL
+ *
+ * Return: void
+ */
+
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp, *prev; 
+
+	temp = prev = head;
+
+	while(temp)
+	{
+		prev = temp;
+		temp = temp->next;
+		free(prev);
+	}
+}
