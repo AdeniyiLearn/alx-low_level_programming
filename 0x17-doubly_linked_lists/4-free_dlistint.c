4a891db58e2ce4322bbb249355deaabe270dfc0f
OluwaSeun Adeniyi <adeniyi.learn@gmail.com>
2023-08-23 14:52:58 +0100

    init

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - Function add node to the end of the list
+ * @head: pointer to the head node as a parameter
+ * Return:  NOthing
+ *
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp, *current;
+
+	temp = current = head;
+	if (head == NULL)
+	{
+		exit (EXIT_FAILURE);
+	}
+
+	while (temp->next != NULL)
+	{
+		current = temp;
+		temp = temp->next;
+		free(current);
+		current = NULL;
+	}
+	free(temp);
+	temp = NULL;
+}
