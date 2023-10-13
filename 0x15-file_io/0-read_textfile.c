d8a8ef7b83cbd82b3929462e14f2fa43de68abbb
OluwaSeun Adeniyi <adeniyi.learn@gmail.com>
2023-10-13 13:55:15 +0100

    initial commit

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * read_textfile - function that converts a binary number to an unsigned int.
+ * @filename: const parameter of type char
+ * @letters: second parameter
+ * Return: converted number or 0 if there is one or more chars in the string b
+ * that is not 0 or 1 b or is NULL failed
+ *
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+	return (score);
+}
