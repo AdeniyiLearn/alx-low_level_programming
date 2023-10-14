c1b90d400ea6e946fb1f521c3096815d07d64e6e
OluwaSeun Adeniyi <adeniyi.learn@gmail.com>
2023-10-14 16:48:19 +0100

    first task

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,15 +2,45 @@
 
 /**
  * read_textfile - function that converts a binary number to an unsigned int.
- * @filename: const parameter of type char
- * @letters: second parameter
- * Return: converted number or 0 if there is one or more chars in the string b
- * that is not 0 or 1 b or is NULL failed
+ * @filename: file to be worked on
+ * @letters: number of letters it should read and print
+ * Return: 0 if filename is NULL, file can not be opened or read, write fails
+ *
  *
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int fd;
+        char *buff;
+	ssize_t bytes_read;
 
-	return (score);
+	buff = malloc(sizeof(char) * letters);
+
+	if (buff == NULL)
+	{
+		return (0);
+	}
+
+        fd = open(filename, O_RDWR);
+
+        if (fd < 0 || filename == NULL)
+        {
+                perror("Error opening file");
+                return 0;
+        }
+
+        bytes_read = read(fd, buff, letters);
+        printf("Read from my file: %s\n", buff);
+
+        if (bytes_read == -1)
+        {
+                perror("Error opening File");
+                return (1);
+        }
+
+        close(fd);
+
+        return (bytes_read);
+
 }
