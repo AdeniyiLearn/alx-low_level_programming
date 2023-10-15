6648636304156c1a8e29ae9dcfa2d350ac2dace6
OluwaSeun Adeniyi <adeniyi.learn@gmail.com>
2023-10-15 17:40:42 +0100

    ; after return

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	ssize_t bytes;
 
 	if (filename == NULL)
-		return (-1)
+		return (-1);
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_RDONLY | O_TRUNC,
 	S_IRUSR | S_IWUSR);
