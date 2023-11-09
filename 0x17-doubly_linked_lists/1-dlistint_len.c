0201576d9d445b678d2831fd7fba7323ea6665a9
OluwaSeun Adeniyi <adeniyi.learn@gmail.com>
2023-11-09 13:19:22 +0100

    remove test

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -9,21 +9,13 @@
 
 size_t dlistint_len(const dlistint_t *h)
 {
-	const dlistint_t *temp;
 	size_t n_node;
 
-	temp = h;
 	n_node = 0;
-
-	if (h == NULL || h == 0)
-	{
-		dprintf(2, "Error: can't malloc\n");
-		return (EXIT_FAILURE);
-	}
-	while (temp != NULL)
+	while (h != NULL)
 	{
 		n_node += 1;
-		temp = temp->next;
+		h = h->next;
 	}
 	return (n_node);
 }
