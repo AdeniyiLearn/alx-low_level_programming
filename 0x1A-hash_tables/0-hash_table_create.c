0d98259082b1a86bca31afc75f49e3f9dede62b5
OluwaSeun Adeniyi <adeniyi.learn@gmail.com>
2024-04-14 01:14:13 +0100

    0x0A dir init

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,14 @@
+#include "hash_table.h"
+
+/**
+ * hash_table_create - Function creates a hash table.
+ * @size: size of the array
+ *
+ * Return:  pointer to the newly created hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+
+	return ();
+}
